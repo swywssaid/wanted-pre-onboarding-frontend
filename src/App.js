978522e9492444fd097eed84012dac16f2eb12eb
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-02-10 23:52:47 +0900

    Fix: Change route method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import TodosPage from "./pages/TodosPage";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
@@ -7,14 +7,14 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <BrowserRouter basename="/wanted-pre-onboarding-frontend">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/todo" element={<TodosPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
