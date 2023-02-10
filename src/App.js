3cc3d8d5ac3d70542e787dd2418ff196c0a616e7
ssaid <94416699+swywssaid@users.noreply.github.com>
2023-02-10 21:24:17 +0900

    Fix: Modify react router dom basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SignInPage from "./pages/SignInPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="wanted-pre-onboarding-frontend">
       <Routes>
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
